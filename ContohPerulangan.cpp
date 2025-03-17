ab64b32059c72af9d6310e43a06ed53e0b02153f
Ghatan55 <ghatanhardanni@gmail.com>
2025-03-17 15:47:06 +0700

    Menambahkan Variabel perulangan do while

--- ContohPerulangan.cpp
+++ ContohPerulangan.cpp
@@ -42,4 +42,22 @@ int main(){
     }
     cout << "Jumlah perulangan = " << perulanganWhile << endl;
     cout << "Nilai n terakhir = " << n << endl;
+
+    cout << "Perulangan do while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
+
+    do
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganDO++;
+    } while (n < 7);
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+    
 }
