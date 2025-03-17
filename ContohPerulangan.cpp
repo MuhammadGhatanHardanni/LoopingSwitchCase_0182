daef444c19815138bbbd8ac0538af978c16bd48e
Ghatan55 <ghatanhardanni@gmail.com>
2025-03-17 15:26:01 +0700

    menambahkan perulangan dengan for

--- ContohPerulangan.cpp
+++ ContohPerulangan.cpp
@@ -7,4 +7,14 @@ int main(){
     int n;
     int perulanganWhile = 0;
     int perulanganDO = 0;
+
+    // Perulangan dengan for
+
+    cout << "Perulangan for pemecah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang " << endl;
+    }
+    cout << "Nilai n terakahir = " << n << endl;
+    cout << endl;
 }
